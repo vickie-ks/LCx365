8241eb6640424e57240b06e2d3430a825d389f93
vickie-ks <vigneshrajan2022@gmail.com>
2024-08-13 22:21:13 -0400

    [CODE] 119-Pascals-Triangle-II

---
+++ lc-archive/119-Pascals-Triangle-II.py
@@ -0,0 +1,11 @@
+from typing import List
+
+class Solution:
+  def getRow(self, rowIndex: int) -> List[int]:
+    row = [1] * (rowIndex + 1)
+    for i in range(1, rowIndex // 2 + 1):
+      row[i] = row[rowIndex - i] = row[i - 1] * (rowIndex - i + 1) // i
+    return row
+
+s = Solution()
+s.getRow(7)
