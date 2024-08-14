20e0a87b2cc33feb4aeede7bce90307f4f5aeb1f
vickie-ks <vigneshrajan2022@gmail.com>
2024-08-13 22:21:25 -0400

    [CODE] 121-Best-Time-to-Buy-and-Sell-Stock

---
+++ lc-archive/121-Best-Time-to-Buy-and-Sell-Stock.py
@@ -0,0 +1,16 @@
+from typing import List
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        min_price = float('inf')
+        max_profit = 0
+        for price in prices:
+            if price < min_price:
+                min_price = price
+            elif price - min_price > max_profit:
+                max_profit = price - min_price 
+        return max_profit
+
+prices = [7, 1, 5, 3, 6, 4]
+solution = Solution()
+print(solution.maxProfit(prices))
